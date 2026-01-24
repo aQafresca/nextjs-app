802c0c39bfec29700e51d90248ac98139219bc98
Siarhei Buiko <innxfect@gmail.com>
2026-01-24 10:41:43 +0300

    JST-2587 feat(features/registrations): add model index file

---
+++ src/features/registration/model/index.ts
@@ -0,0 +1,9 @@
+export * from './constants/formOptions';
+export { addressSchema } from './schema/address.shema';
+export { emailSchema } from './schema/email.schema';
+export { nameSchema } from './schema/name.schema';
+export { passwordSchema } from './schema/password.shema';
+export { phoneSchema } from './schema/phoneShema';
+export { radioSchema } from './schema/radio.schema';
+export * from './schema/registrationFormSchema';
+export * from './types/formFields';
