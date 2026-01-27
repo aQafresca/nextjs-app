b50caf73d7c3b74f974c26b233ec8c3a01680188
Siarhei Buiko <innxfect@gmail.com>
2026-01-27 18:10:56 +0300

    JST-2587 feat(features/products): add product list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import type { IProductListPromiseProps } from '@/features/product-list/model';
 import { HomePage } from '@/pages/home';
 
-export default function Home() {
-  return <HomePage />;
+export default async function Home(props: IProductListPromiseProps) {
+  const searchParams = await props.searchParams;
+
+  const page = Math.max(1, Number(searchParams.page) || 1);
+  const pageSize = Math.max(1, Number(searchParams.pageSize) || 10);
+
+  return <HomePage page={page} pageSize={pageSize} />;
 }
---
+++ src/features/product-list/ui/productList.tsx
@@ -0,0 +1,51 @@
+import { EmptyState } from '@/shared/ui/empty-state';
+import { PaginationWithLinks } from '@/shared/ui/pagination-with-links';
+
+import { productsService } from '@/entities/products/api';
+import { ProductCard } from '@/entities/products/ui';
+import type { IProductListProps } from '@/features/product-list/model';
+
+export const ProductList = async ({ page, pageSize }: IProductListProps) => {
+  const skip = (page - 1) * pageSize;
+
+  const data = await productsService.getAllProducts({ skip, limit: pageSize });
+  const { total, products } = data;
+
+  const totalPages = Math.ceil(total / pageSize);
+
+  const isTotalEmpty = total === 0;
+  const isOutOfRange = page > totalPages && total > 0;
+
+  return (
+    <div
+      className={
+        'flex-1 flex flex-col items-center justify-between gap-6 mx-auto w-full py-6'
+      }
+    >
+      {isOutOfRange ? <EmptyState message={'No products found'} /> : null}
+
+      {!isTotalEmpty && !isOutOfRange && (
+        <>
+          <div className={'flex flex-wrap justify-center gap-5 w-full'}>
+            {products.map((product, index) => (
+              <ProductCard
+                key={product.id}
+                product={product}
+                isPriority={index === 0}
+              />
+            ))}
+          </div>
+
+          {totalPages > 1 && (
+            <PaginationWithLinks
+              page={page}
+              pageSize={pageSize}
+              totalCount={total}
+              navigationMode="router"
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
