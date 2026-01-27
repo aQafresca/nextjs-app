b365f4a2143efe2ec97696a90060030dca6c592d
Siarhei Buiko <innxfect@gmail.com>
2026-01-27 18:08:30 +0300

    JST-2587 feat(entities/products): add cart

---
+++ src/entities/products/ui/index.ts
@@ -0,0 +1,5 @@
+export { ActionButton } from './actionButton';
+export { ProductCard } from './card';
+export { PriceDisplay } from './priceDisplay';
+export { ToggleAddToCart } from './toggleAddToCart';
+export { ToggleAddToFavorite } from './toggleAddToFavorite';
