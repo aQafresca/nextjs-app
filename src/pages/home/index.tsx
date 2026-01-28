3f95311b1fd54f997785f73b671d88f5b921deb0
Siarhei Buiko <innxfect@gmail.com>
2026-01-28 11:15:17 +0300

    JST-2587 feat(pages/home): implement query

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,10 @@
 import type { IProductListProps } from '@/features/product-list/model';
 import { ProductList } from '@/features/product-list/ui';
 
-export const HomePage = ({ page, pageSize }: IProductListProps) => {
+export const HomePage = ({ page, pageSize, query }: IProductListProps) => {
   return (
     <div className={'flex-1 flex flex-col'}>
-      <ProductList page={page} pageSize={pageSize} />
+      <ProductList page={page} pageSize={pageSize} query={query} />
     </div>
   );
 };
