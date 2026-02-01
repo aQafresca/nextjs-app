34994728b4d1604ccdbf06a3d8aabf20482e86f3
Siarhei Buiko <innxfect@gmail.com>
2026-02-01 13:25:59 +0300

    JST-2587 feat(entities/products): add toggle add to favorite button logic

--- src/entities/products/ui/toggleAddToFavorite.tsx
+++ src/entities/products/ui/toggleAddToFavorite.tsx
@@ -2,18 +2,29 @@
 
 import { Heart } from 'lucide-react';
 
+import { useFavoriteStore } from '@/entities/favorites/model';
+import type { IProduct } from '@/entities/products/model';
 import { ActionButton } from '@/entities/products/ui';
 
-export const ToggleAddToFavorite = ({ productId }: { productId: number }) => {
-  const handle = () => {
-    console.log('Added to Favorites:', productId);
-  };
+interface Props {
+  product: IProduct;
+}
+
+export const ToggleAddToFavorite = ({ product }: Props) => {
+  const { items, toggle, hasHydrated } = useFavoriteStore();
+
+  const isFavorite =
+    hasHydrated && items.some((item) => item.id === product.id);
 
   return (
     <ActionButton
-      icon={<Heart size={20} />}
-      onClick={handle}
-      className="hover:text-red-500 absolute top-3 right-3"
+      icon={<Heart size={20} className={isFavorite ? 'fill-current' : ''} />}
+      onClick={() => {
+        toggle(product);
+      }}
+      className={`absolute top-3 right-3 transition-colors ${
+        isFavorite ? 'text-red-500' : 'hover:text-red-500'
+      }`}
     />
   );
 };
