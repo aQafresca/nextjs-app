36e003de3819ff1e1f41af0972342d7d29e5ed6f
Siarhei Buiko <innxfect@gmail.com>
2026-02-01 09:32:30 +0300

    JST-2587 refactor(entities/products): update types

--- src/entities/products/model/types.ts
+++ src/entities/products/model/types.ts
@@ -10,6 +10,39 @@ export interface IProduct {
   category: string;
   thumbnail: string;
   images: string[];
+
+  tags?: string[];
+  sku?: string;
+  weight?: number;
+  dimensions?: IDimensions;
+  warrantyInformation?: string;
+  shippingInformation?: string;
+  availabilityStatus?: string;
+  reviews?: IReview[];
+  returnPolicy?: string;
+  minimumOrderQuantity?: number;
+  meta?: IMeta;
+}
+
+export interface IReview {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail?: string;
+}
+
+export interface IDimensions {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export interface IMeta {
+  createdAt: string;
+  updatedAt: string;
+  barcode: string;
+  qrCode: string;
 }
 
 export interface IProductsResponse {
